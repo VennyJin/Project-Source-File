514db1a20e39f5f9e55c79f212376fde5a17c05b
venny <vennyjin@outlook.com>
2020-11-09 14:50:19 -0800

    put camera acquisition to main function

--- src/Acquisition.cpp
+++ src/Acquisition.cpp
@@ -1,19 +1,3 @@
-//=============================================================================
-// Copyright (c) 2001-2019 FLIR Systems, Inc. All Rights Reserved.
-//
-// This software is the confidential and proprietary information of FLIR
-// Integrated Imaging Solutions, Inc. ("Confidential Information"). You
-// shall not disclose such Confidential Information and shall use it only in
-// accordance with the terms of the license agreement you entered into
-// with FLIR Integrated Imaging Solutions, Inc. (FLIR).
-//
-// FLIR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
-// SOFTWARE, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-// PURPOSE, OR NON-INFRINGEMENT. FLIR SHALL NOT BE LIABLE FOR ANY DAMAGES
-// SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
-// THIS SOFTWARE OR ITS DERIVATIVES.
-//=============================================================================
 
 /**
  *  @example Acquisition.cpp
@@ -205,7 +189,7 @@ int AcquireImages(CameraPtr pCam, INodeMap& nodeMap, INodeMap& nodeMapTLDevice)
         cout << endl;
 
         // Retrieve, convert, and save images
-        const unsigned int k_numImages = 10;
+        const unsigned int k_numImages = 1;
 
         for (unsigned int imageCnt = 0; imageCnt < k_numImages; imageCnt++)
         {
@@ -470,113 +454,3 @@ int InitializateCamera(void)
     }
     return result;
 }
-
-
-// Example entry point; please see Enumeration example for more in-depth
-// comments on preparing and cleaning up the system.
-
-//int main(int /*argc*/, char** /*argv*/)
-//{
-    // Since this application saves images in the current folder
-    // we must ensure that we have permission to write to this folder.
-    // If we do not have permission, fail right away.
-//    FILE* tempFile = fopen("test.txt", "w+");
-//    if (tempFile == nullptr)
-//    {
-//        cout << "Failed to create file in current folder.  Please check "
-//                "permissions."
-//             << endl;
-//        cout << "Press Enter to exit..." << endl;
-//        getchar();
-//        return -1;
-//    }
-//    fclose(tempFile);
-//    remove("test.txt");
-//
-//    // Print application build information
-//    cout << "Application build date: " << __DATE__ << " " << __TIME__ << endl << endl;
-//
-//    // Retrieve singleton reference to system object
-//    SystemPtr system = System::GetInstance();
-//
-//    // Print out current library version
-//    const LibraryVersion spinnakerLibraryVersion = system->GetLibraryVersion();
-//    cout << "Spinnaker library version: " << spinnakerLibraryVersion.major << "." << spinnakerLibraryVersion.minor
-//         << "." << spinnakerLibraryVersion.type << "." << spinnakerLibraryVersion.build << endl
-//         << endl;
-//
-//    // Retrieve list of cameras from the system
-//    CameraList camList = system->GetCameras();
-//
-//    const unsigned int numCameras = camList.GetSize();
-//
-//    cout << "Number of cameras detected: " << numCameras << endl << endl;
-//
-//    // Finish if there are no cameras
-//    if (numCameras == 0)
-//    {
-//        // Clear camera list before releasing system
-//        camList.Clear();
-//
-//        // Release system
-//        system->ReleaseInstance();
-//
-//        cout << "Not enough cameras!" << endl;
-//        cout << "Done! Press Enter to exit..." << endl;
-//        getchar();
-//
-//        return -1;
-//     }
-//
-
-    // Create shared pointer to camera
-    //
-    // *** NOTES ***
-    // The CameraPtr object is a shared pointer, and will generally clean itself
-    // up upon exiting its scope. However, if a shared pointer is created in the
-    // same scope that a system object is explicitly released (i.e. this scope),
-    // the reference to the shared point must be broken manually.
-    //
-    // *** LATER ***
-    // Shared pointers can be terminated manually by assigning them to nullptr.
-    // This keeps releasing the system from throwing an exception.
-    //
-//    CameraPtr pCam = nullptr;
-//
-//    int result = 0;
-//
-//    // Run example on each camera
-//    for (unsigned int i = 0; i < numCameras; i++)
-//    {
-//        // Select camera
-//        pCam = camList.GetByIndex(i);
-//
-//        cout << endl << "Running program for camera " << i << "..." << endl;
-//
-//        // Run example
-//        result = result | RunSingleCamera(pCam);
-//
-//        cout << "Camera " << i << " program complete..." << endl << endl;
-//    }
-//
-//    //
-//    // Release reference to the camera
-//    //
-//    // *** NOTES ***
-//    // Had the CameraPtr object been created within the for-loop, it would not
-//    // be necessary to manually break the reference because the shared pointer
-//    // would have automatically cleaned itself up upon exiting the loop.
-//    //
-//    pCam = nullptr;
-//
-//    // Clear camera list before releasing system
-//    camList.Clear();
-//
-//    // Release system
-//    system->ReleaseInstance();
-//
-//    cout << endl << "Done! Press Enter to exit..." << endl;
-//    getchar();
-//
-//    return result;
-//}
--- src/main.cpp
+++ src/main.cpp
@@ -114,6 +114,106 @@ aj::Project CreateProject(unsigned short sequenceID=1, unsigned int sequenceRepe
 
 
 int main(int argc, char **argv) {
-    InitializateCamera();
+    // Since this application saves images in the current folder
+    // we must ensure that we have permission to write to this folder.
+    // If we do not have permission, fail right away.
+    FILE* tempFile = fopen("test.txt", "w+");
+    if (tempFile == nullptr)
+    {
+        cout << "Failed to create file in current folder.  Please check "
+                "permissions."
+             << endl;
+        cout << "Press Enter to exit..." << endl;
+        getchar();
+        return -1;
+    }
+    fclose(tempFile);
+    remove("test.txt");
+
+    // Print application build information
+    cout << "Application build date: " << __DATE__ << " " << __TIME__ << endl << endl;
+
+    // Retrieve singleton reference to system object
+    SystemPtr system = System::GetInstance();
+
+    // Print out current library version
+    const LibraryVersion spinnakerLibraryVersion = system->GetLibraryVersion();
+    cout << "Spinnaker library version: " << spinnakerLibraryVersion.major << "." << spinnakerLibraryVersion.minor
+         << "." << spinnakerLibraryVersion.type << "." << spinnakerLibraryVersion.build << endl
+         << endl;
+
+    // Retrieve list of cameras from the system
+    CameraList camList = system->GetCameras();
+
+    const unsigned int numCameras = camList.GetSize();
+
+    cout << "Number of cameras detected: " << numCameras << endl << endl;
+
+    // Finish if there are no cameras
+    if (numCameras == 0)
+    {
+        // Clear camera list before releasing system
+        camList.Clear();
+
+        // Release system
+        system->ReleaseInstance();
+
+        cout << "Not enough cameras!" << endl;
+        cout << "Done! Press Enter to exit..." << endl;
+        getchar();
+
+        return -1;
+     }
+
+
+  // Create shared pointer to camera
+  //
+  // *** NOTES ***
+  // The CameraPtr object is a shared pointer, and will generally clean itself
+  // up upon exiting its scope. However, if a shared pointer is created in the
+  // same scope that a system object is explicitly released (i.e. this scope),
+  // the reference to the shared point must be broken manually.
+  //
+  // *** LATER ***
+  // Shared pointers can be terminated manually by assigning them to nullptr.
+  // This keeps releasing the system from throwing an exception.
+  //
+    CameraPtr pCam = nullptr;
+
+    int result = 0;
+
+    // Run example on each camera
+    for (unsigned int i = 0; i < numCameras; i++)
+    {
+        // Select camera
+        pCam = camList.GetByIndex(i);
+
+        cout << endl << "Running program for camera " << i << "..." << endl;
+
+        // Run example
+        result = result | RunSingleCamera(pCam);
+
+        cout << "Camera " << i << " program complete..." << endl << endl;
+    }
+
+    //
+    // Release reference to the camera
+    //
+    // *** NOTES ***
+    // Had the CameraPtr object been created within the for-loop, it would not
+    // be necessary to manually break the reference because the shared pointer
+    // would have automatically cleaned itself up upon exiting the loop.
+    //
+    pCam = nullptr;
+
+    // Clear camera list before releasing system
+    camList.Clear();
+
+    // Release system
+    system->ReleaseInstance();
+
+    cout << endl << "Done! Press Enter to exit..." << endl;
+    //getchar();
+
     return RunExample(&CreateProject, argc, argv);
 }
